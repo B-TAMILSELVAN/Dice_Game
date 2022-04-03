c6ccc45645cd3c7d6e821aea181e3870ee78f1c3
B-TAILSELVAN <87471378+B-TAMILSELVAN@users.noreply.github.com>
2022-04-03 12:49:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let randomNumber1=Math.floor(Math.random() * 6) + 1;
+let image1="images/dice"+randomNumber1+".png";
+
+
+document.querySelector(".dice .img1").setAttribute("src",image1);
+let randomNumber2=Math.floor(Math.random() * 6) + 1;
+let image2="images/dice"+randomNumber2+".png";
+document.querySelector(".dice .img2").setAttribute("src",image2)
+if(randomNumber1>randomNumber2)
+document.querySelector("h1").textContent="player1 wins 🚩";
+else if (randomNumber2>randomNumber1) {
+document.querySelector("h1").textContent="player2 wins 🚩";
+
+}
+else{
+  document.querySelector("h1").textContent="Draw!";
+}
